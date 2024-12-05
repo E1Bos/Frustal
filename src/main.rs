8b8b80bdc8c2d7fe4f10d3d56a2e37fc4eb120af
E1Bos <lboscolo68@gmail.com>
2024-12-05 21:37:25 +0000

    simplified

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
 mod args;
-use args::Args;
-
 mod fractals;
 mod renderer;
 
-use winit::error::EventLoopError;
-use num::Complex;
+use args::Args;
+use renderer::RendererRunner;
 
-// Example position
-// upper left: -2.5690780055377322+1.4248376056487224i, 1.3964634498916704-2.5407038497806793i
-
-fn main() -> Result<(), EventLoopError> {
-    let renderer_runner = renderer::RendererRunner::new().with_args(Args::new(
-        500,
-        500,
-        300,
-        false,
-        Complex::new(-2.5, 1.5),
-        Complex::new(1.5, -2.5),
-    ));
-    renderer_runner.run()?;
-
+fn main() -> Result<(), pixels::Error> {
+    let args = Args::default()
+        .with_size(800, 600)
+        .with_max_iterations(200);
+    let runner = RendererRunner::new()?;
+    runner.with_args(args).run()?;
     Ok(())
 }
